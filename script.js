f74122feb15b54249b3a4e5723681e352fe2e87c
Jakub Pniak <jpniak@o2.pl>
2021-06-16 22:07:01 +0200

    Add function addTask

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('form');
+const ul = document.querySelector('ul');
+const tasksNumber = document.querySelector('h2 span');
+const tasksList = document.getElementsByClassName('task');
+const input = document.querySelector('input');
+
+
+/* Adding a new task to the list */
+const addTask = function(event) {
+ event.preventDefault()
+  
+ const titleTask = input.value;
+ if (titleTask === "") return;
+ const task = document.createElement('li');
+ task.className = 'task';
+ task.innerHTML = titleTask;
+ ul.appendChild(task);
+ input.value = "";
+ }
+
+form.addEventListener('submit', addTask)
